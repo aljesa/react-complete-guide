0a3abcb1d094ef8ad55116c677d2842f173ac363
bexalho <aljesa.hoxha@bexeo.com>
2022-10-03 15:09:33 +0200

    show hide on click button expenses form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,19 @@
-import { useState } from "react";
-
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-    //this is called react hooks - useState
-    const [title, setTitle] = useState(props.title); //this returns array
-
-    function clickHandler() {
-        setTitle('Updated!');
-    }
 
     return (
-        <Card className="expense-item">
-            <ExpenseDate date={props.date}/>
-            <div className="expense-item__description">
-                <h2>{title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-            <button onClick={clickHandler}>Change Title</button>
-        </Card>
+        <li>
+            <Card className="expense-item">
+                <ExpenseDate date={props.date}/>
+                <div className="expense-item__description">
+                    <h2>{props.title}</h2>
+                    <div className="expense-item__price">${props.amount}</div>
+                </div>
+            </Card>
+        </li>
     );
 }
 
